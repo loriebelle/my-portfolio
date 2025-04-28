6204598414a44edfe45c5ed019475456accf521a
Lorie <rie_bsly@icloud.com>
2025-04-27 22:24:05 -0700

    added a bg transition and custom cursor

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
-function portfolio_enqueues() {
+function portfolio_enqueues()
+{
     wp_enqueue_style(
         'portfolio-style',
         get_stylesheet_uri(),
@@ -9,16 +10,25 @@ function portfolio_enqueues() {
     );
 
     wp_enqueue_style(
-        'mindset-normalize',
+        'portfolio-normalize',
         'https://unpkg.com/@csstools/normalize.css',
         array(),
         '12.1.0'
     );
+
+    wp_enqueue_script(
+        'portfolio-custom-cursor',
+        get_theme_file_uri('assets/js/custom-cursor.js'),
+        array(),
+        wp_get_theme()->get('Version'),
+        array('strategy' => 'defer')
+    );
 }
 add_action('wp_enqueue_scripts', 'portfolio_enqueues');
 
-function portfolio_setup() {
-    add_editor_style( get_stylesheet_uri() );
+function portfolio_setup()
+{
+    add_editor_style(get_stylesheet_uri());
 }
 add_action('after_setup_theme', 'portfolio_setup');
 
